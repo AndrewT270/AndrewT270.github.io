dabf7a6836ac4cce9a8cfd5c2344d2cf7768038a
AndrewT270 <andrew.tran1@jpschools.org>
2025-06-23 16:17:16 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -41,7 +41,7 @@ var init = function (window) {
         // TODO 7 : Use a loop to create multiple circles
         for (var amountCirc = 0; amountCirc < 100; amountCirc++) {
             drawCircle();
-}
+        }
 
 
 
@@ -56,17 +56,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-    
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
           
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
           
             
             // TODO 8 / TODO 9 : Iterate over the array
-           for (var i = 0; i < circles.length; i++) {
-            physikz.updatePosition(circles[i]);
-            game.checkCirclePosition(circles[i]);
-        }
+    
     
         /* 
         This Function should check the position of a circle that is passed to the 
@@ -110,10 +111,9 @@ var init = function (window) {
         app.addUpdateable(window.opspark.game);
     }
 };
-
 // DO NOT REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
-}
+};
